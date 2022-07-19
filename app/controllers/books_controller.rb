0d2b9fb956f8d50c02fb0f9522869bfb9ccd18ab
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-07-19 13:18:23 +0000

    [add36]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to post_books_path
   end
 
   def index
@@ -12,5 +20,11 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+    private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
