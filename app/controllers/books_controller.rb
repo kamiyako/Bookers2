43a7bf4fc55e47ad2002e659aa793f771938130d
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-07-19 15:25:49 +0000

    [add37]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,20 +6,45 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to post_books_path
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to books_path
+    else
+      render books_path
+    end
   end
+  
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @books.order("id")
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id]) 
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to books_path(@book.id) 
+    else
+    render :edit
+    end
   end
   
   def destroy
+    book = Book.find(params[:id]) 
+    if book.destroy 
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
+    end
   end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about", to: "homes#about", as: "about"
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
