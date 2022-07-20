5f7b18755df95ef62d3bdc2fb230dcaa6ceba6e9
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-07-20 12:26:51 +0000

    [add58]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id]) 
   end
   
-  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "User was successfully updated."
+    redirect_to user_path(@user.id)
+    else
+    render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
