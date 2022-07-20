cf656267f7ace4db60ded363120df7d92325b033
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-07-20 05:55:12 +0000

    [add47]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id]) 
+    @books = @user.books   #1userの全てのbooks
   end
 
   def edit
+    @user = User.find(params[:id]) 
   end
+  
+  
+
+  
 end
