a3e6b99da5554499f352e8d0c6bd600875483673
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-07-25 08:59:01 +0000

    [update]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   devise_for :users
-  get "homes/about", to: "homes#about", as: "about"
+  get "home/about", to: "homes#about", as: "about"
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
