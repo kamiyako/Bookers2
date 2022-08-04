9ba7e5dfca5b3ef0ee1375c40e3b06930fd85f31
kamiyako <108565075+kamiyako@users.noreply.github.com>
2022-08-04 08:14:43 +0000

    [ajax]like

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
-
+before_action :favorited_params
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path(book)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to books_path(book)
   end
 
+  private
+
+  def favorited_params
+    @book = Book.find(params[:book_id])
+  end
 end
